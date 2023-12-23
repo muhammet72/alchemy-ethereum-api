fb427e02b3168e1bb640653de317e6d3417a14af
isik <code213isik@gmail.com>
2023-12-23 10:08:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Setup: npm install @alch/alchemy-sdk
+const { Network, Alchemy } = require("alchemy-sdk");
+
+// Optional Config object, but defaults to demo api-key and eth-mainnet.
+const settings = {
+  apiKey: "", //Add your Alchemy API Key.
+  network: Network.ETH_SEPOLIA,
+};
+
+const alchemy = new Alchemy(settings);
+
+async function main() {
+  const latestBlock = await alchemy.core.getBlockNumber();
+  console.log("The latest block number is", latestBlock);
+}
+
+main();
